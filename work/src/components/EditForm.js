07c5112975e3f37c8a19ddea1d7a20ca89722755
krenee411 <kelci.morgan@bryanuniversity.edu>
2022-03-17 18:30:11 -0500

    HELP ADDED

---
+++ work/src/components/EditForm.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import {useState} from 'react';
+
+function EditTodoForm({todo}) {
+    
+
+    const [userInput, setUserInput] = useState("");
+    
+    
+//     const validateForm = (e) => {
+//         e.preventDefault();
+//         if (userInput !== "") {
+//             editTodo(todo, userInput);
+//         } 
+
+//     const cancel = cancelBtn;
+//     cancel();
+// }
+
+const updateClick = (id) => {
+    axios.put(`http://localhost:9000/UpdateTodo/:${id}`,{
+        id: todo.id,
+        text: userInput
+    })
+}
+const changeState = (e) => {
+    setUserInput(e.target.value);
+}
+
+
+    return (
+        <form>
+            <label>Edit Todo: </label>
+            <input type='text' name="text" value ={userInput} onChange ={changeState}></input>
+            <button type='submit' onClick={()=>{updateClick(todo.id)}}>Update</button> 
+            
+        </form>
+    );
+}
+
+
+export default EditTodoForm;
